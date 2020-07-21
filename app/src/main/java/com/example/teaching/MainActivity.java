66117b69ecb783b5931e83e89cc6f324f6fb5661
fredrikmejs <s185130@student.dtu.dk>
2020-07-21 13:00:38 +0200

    contiuned work on UI for burger

--- app/src/main/java/com/example/teaching/MainActivity.java
+++ app/src/main/java/com/example/teaching/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.teaching;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button burger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        burger = findViewById(R.id.burger_button);
+        burger.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == burger){
+            Intent intent = new Intent(this,Burger.class);
+            startActivity(intent);
+        }
     }
 }
